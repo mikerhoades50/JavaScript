3420dad801ff7df0c637b90d51254afae4ba1df5
Mike Rhoades <michael.d.rhoades@gmail.com>
2026-03-12 18:10:11 -0500

    initial commit

---
+++ Module-01/Exercise-03/js/index.js
@@ -0,0 +1,8 @@
+let x = prompt("Enter the first number:");
+let y = prompt("Enter the second number:");
+
+//debugger;
+let result = parseInt(x) + parseInt(y);
+
+console.log("The result of adding " + x + " and " + y + " is: " + result); 
+alert("The result of adding " + x + " and " + y + " is: " + result);
---
+++ Module-01/Exercise-04/js/primitive-datatypes.js
@@ -0,0 +1,8 @@
+console.log(5,typeof 5);
+console.log(5.2, typeof 5.2);
+console.log("Hello World", typeof "Hello World");
+console.log(true, typeof true);
+console.log(false, typeof false);
+console.log(null, typeof null);
+console.log(undefined, typeof undefined);
+
